dffec7dc6673c264701fd39ed6bd924f3c2c15ae
Donghwan Kim <flowersinthesand@gmail.com>
2018-11-04 19:25:06 +0900

    Use Cettia 1.2.0

--- src/main/java/io/cettia/starter/CettiaConfigListener.java
+++ src/main/java/io/cettia/starter/CettiaConfigListener.java
@@ -5,6 +5,7 @@ import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.ITopic;
 import com.hazelcast.instance.HazelcastInstanceFactory;
 import io.cettia.ClusteredServer;
+import io.cettia.Sentence;
 import io.cettia.ServerSocket;
 import io.cettia.asity.action.Action;
 import io.cettia.asity.bridge.jwa1.AsityServerEndpoint;
@@ -18,6 +19,8 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.ServletRegistration;
 import javax.servlet.annotation.WebListener;
 import javax.websocket.DeploymentException;
+import javax.websocket.HandshakeResponse;
+import javax.websocket.server.HandshakeRequest;
 import javax.websocket.server.ServerContainer;
 import javax.websocket.server.ServerEndpointConfig;
 import java.util.Map;
@@ -26,7 +29,14 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
+import static io.cettia.ServerSocketPredicates.all;
+import static io.cettia.ServerSocketPredicates.attr;
+import static io.cettia.ServerSocketPredicates.id;
+import static io.cettia.ServerSocketPredicates.tag;
 import static java.util.concurrent.TimeUnit.SECONDS;
+
+//import io.cettia.DefaultServer;
+//import io.cettia.Server;
 
 @WebListener
 public class CettiaConfigListener implements ServletContextListener {
@@ -34,19 +44,19 @@ public class CettiaConfigListener implements ServletContextListener {
   public void contextInitialized(ServletContextEvent event) {
     // Cettia part
     // If you don't want to form a cluster,
-    // replace the following line with `Server server = new DefaultServer();`
+    // replace the following line with 'Server server = new DefaultServer();'
     ClusteredServer server = new ClusteredServer();
     HttpTransportServer httpAction = new HttpTransportServer().ontransport(server);
     WebSocketTransportServer wsAction = new WebSocketTransportServer().ontransport(server);
 
-    // If a client opens a socket, the server creates and passes a ServerSocket to socket handlers
+    // If a client opens a socket, the server creates and passes a socket to socket handlers
     server.onsocket((ServerSocket socket) -> {
       // ## Socket Lifecycle
       Action<Void> logState = v -> System.out.println(socket + " " + socket.state());
       socket.onopen(logState).onclose(logState).ondelete(logState);
 
       // ## Sending and Receiving Events
-      // An `echo` event handler where any received echo event is sent back
+      // An 'echo' event handler where any received echo event is sent back
       socket.on("echo", data -> socket.send("echo", data));
 
       // ## Attributes and Tags
@@ -61,31 +71,30 @@ public class CettiaConfigListener implements ServletContextListener {
       }
 
       // ## Working with Sockets
-      // A `chat` event handler to send a given chat event to every socket in every server in the cluster
-      socket.on("chat", data -> server.all().send("chat", data));
+      // A 'chat' event handler to send a given chat event to every socket in every server in the cluster
+      // ServerSocketPredicates#all() returns a predicate that always matches
+      socket.on("chat", data -> server.find(all()).send("chat", data));
 
-      // ## Finder Methods and Sentence
+      // ## Advanced Sockets Handling
       if (username != null) {
+        // A fluent interface to help deal with a group of sockets
+        Sentence user = server.find(attr("username", username));
         // A myself event handler to send a given myself event to sockets whose username is the same
         socket.on("myself", data -> {
-          // Find sockets by the attribute and deal with them directly
-          server.find(s -> username.equals(s.get("username"))).execute(s -> s.send("myself", data));
+          // You can directly handle each socket through the execute method of Sentence if needed
+          user.execute(s -> s.send("myself", data));
         });
 
-        // How to allow only one socket per username
+        // Limits only one socket per user
         boolean onlyOneSocket = Boolean.parseBoolean(findParam(socket.uri(), "onlyOneSocket"));
         if (onlyOneSocket) {
-          // Finds sockets whose username is the same except this socket
-          String me = socket.id();
-          server.find(s -> username.equals(s.get("username")) && !me.equals(s.id()))
-          // Sends a `signout` event to prevent reconnection and closes a connection
-          .send("signout").close();
-          // As of 1.2, it can be written more concisely
-          // `server.byAttr("username", username).exclude(socket).send("signout").close();`
+          // Finds sockets whose username is the same other than this socket and
+          // sends a 'signout' event to prevent reconnection and closes a connection
+          user.find(id(socket).negate()).send("signout").close();
         }
       }
 
-      // ## Recovering Missed Events
+      // ## Disconnection Handling
       Queue<Object[]> queue = new ConcurrentLinkedQueue<>();
       // Caches events that fail to send due to disconnection
       socket.oncache(args -> queue.offer(args));
@@ -106,12 +115,12 @@ public class CettiaConfigListener implements ServletContextListener {
     // ## Working with Sockets
     // To deal with sockets, inject the server wherever you want
     ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-    // Sends a welcome event to sockets representing user not signed in every 5 seconds
-    executor.scheduleAtFixedRate(() -> server.byTag("nonmember").send("welcome"), 0, 5, SECONDS);
+    // Sends a 'welcome' event to sockets representing user not signed in every 5 seconds
+    executor.scheduleAtFixedRate(() -> server.find(tag("nonmember")).send("welcome"), 0, 5, SECONDS);
 
     // ## Plugging Into the Web Framework
     // Cettia is designed to run on any web framework seamlessly on the JVM
-    // Note how `httpAction` and `wsAction` are plugged into Servlet and Java API for Websocket
+    // Note how 'httpAction' and 'wsAction' are plugged into Servlet and Java API for Websocket
     ServletContext context = event.getServletContext();
     AsityServlet asityServlet = new AsityServlet().onhttp(/* ㅇㅅㅇ */ httpAction);
     ServletRegistration.Dynamic reg = context.addServlet(AsityServlet.class.getName(), asityServlet);
@@ -122,9 +131,13 @@ public class CettiaConfigListener implements ServletContextListener {
     ServerEndpointConfig.Configurator configurator = new ServerEndpointConfig.Configurator() {
       @Override
       public <T> T getEndpointInstance(Class<T> endpointClass) {
-        AsityServerEndpoint asityServerEndpoint = new AsityServerEndpoint();
-        asityServerEndpoint.onwebsocket(/* ㅇㅅㅇ */ wsAction);
+        AsityServerEndpoint asityServerEndpoint = new AsityServerEndpoint().onwebsocket(/* ㅇㅅㅇ */ wsAction);
         return endpointClass.cast(asityServerEndpoint);
+      }
+
+      @Override
+      public void modifyHandshake(ServerEndpointConfig config, HandshakeRequest request, HandshakeResponse response) {
+        config.getUserProperties().put(HandshakeRequest.class.getName(), request);
       }
     };
     try {
@@ -135,7 +148,7 @@ public class CettiaConfigListener implements ServletContextListener {
 
     // ## Scaling a Cettia Application
     // Any publish-subscribe messaging system can be used to scale a Cettia application horizontally,
-    // and it doesn’t require any modification in the existing application.
+    // and it doesn't require any modification in the existing application.
     HazelcastInstance hazelcast = HazelcastInstanceFactory.newHazelcastInstance(new Config());
     ITopic<Map<String, Object>> topic = hazelcast.getTopic("cettia");
     // It publishes messages given by the server
@@ -157,5 +170,4 @@ public class CettiaConfigListener implements ServletContextListener {
 
   @Override
   public void contextDestroyed(ServletContextEvent sce) {}
-
 }
